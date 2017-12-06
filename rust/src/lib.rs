be12270e45393e7590cf260919861a61e290e0af
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2017-12-06 16:51:18 +0000

    Push "computation" into separate thread
    This will allow Tornado to handle multiple clients at once.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,5 +1,7 @@
-use std::mem;
+use std::{mem, thread};
 use std::os::raw::{c_char, c_void};
+use std::ops::Deref;
+use std::ptr;
 
 type Callback = extern fn(*mut c_void, SizedString);
 
@@ -21,9 +23,35 @@ impl From<String> for SizedString {
     }
 }
 
+struct Sendable(*mut c_void);
+
+unsafe impl Send for Sendable {}
+
+impl Deref for Sendable {
+    type Target = *mut c_void;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 #[no_mangle]
-pub unsafe extern "C" fn process(bytes: *const c_char, len: usize, cb: Callback, h: *mut c_void) {
-    let s = String::from_raw_parts(bytes as *mut _, len, len);
-    let ss = SizedString::from(s);
-    cb(h, ss);
+pub unsafe extern "C" fn process(
+    bytes: *mut u8, len: usize, cb: Callback, h: *mut c_void)
+{
+    // copy the bytes because we need ownership over them
+    let mut copied_bytes = Vec::with_capacity(len);
+    copied_bytes.set_len(len);
+    ptr::copy(bytes, copied_bytes.as_mut_ptr(), len);
+
+    // create owned string so we can pass it between threads
+    let s = String::from_utf8(copied_bytes).unwrap();
+
+    // make handle `Send`able too
+    let s_h = Sendable(h);
+
+    thread::spawn(move || {
+        let ss = SizedString::from(s);
+        cb(*s_h, ss);
+    });
 }
